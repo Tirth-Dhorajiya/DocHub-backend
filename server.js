503260f4e5283f5752fb5d084a05b1d4ba65029f
Tirth-Dhorajiya <dhorajiyatirth3012@gmail.com>
2025-03-13 17:18:36 +0530

    Updated server.js and vercel.json

--- server.js
+++ server.js
@@ -67,5 +67,8 @@ app.get("/api/test", (req, res) => {
   res.send({ message: "Backend is working!" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Fix for Vercel Deployment: Export the Serverless Function
+module.exports = app;
